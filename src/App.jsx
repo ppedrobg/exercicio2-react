5a391210bba2851a5910462b3af84b24d752d355
ppedrobg <rm551061@fiap.com.br>
2023-09-14 23:07:28 -0300

    commit para funcionamento no vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet} from "react-router-dom";
 import Cabecalho from "./components/Cabecalho";
-import Rodape from "./components/Rodape";
+import Rodape from "./components/rodape";
 
 export default function App() {
   
