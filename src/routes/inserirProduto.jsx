6f3b93c7be512d3ba0898ee33176aee01f274068
ppedrobg <rm551061@fiap.com.br>
2023-09-26 15:04:42 -0300

    commit antes da aula

--- src/routes/inserirProduto.jsx
+++ src/routes/inserirProduto.jsx
@@ -1,55 +1,112 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function inserirProduto(){
-    document.title = "CADASTRAR";   
+export default function inserirProduto() {
+  document.title = "CADASTRAR";
 
-    const [listaLocalProdutos, setListaLocalProdutos] = useState([{}]);
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        fetch("http://localhost:5000/produtos",{
-            method:"GET",
-            headers:{
-                "Content-Type" : "application/json",
-            }
-        })
-        .then((response)=> response.json())
-        .then((data)=>{
-            setListaLocalProdutos(data);
-        }).catch(error => console.log(error));
+  const [listaLocalProdutos, setListaLocalProdutos] = useState([{}]);
+  let novoId;
+  useEffect(() => {
+    fetch("http://localhost:5000/produtos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setListaLocalProdutos(data);
+      })
+      .catch((error) => console.log(error));
 
-        const novoId = (listaLocalProdutos[listaLocalProdutos.length - 1].id + 1)
-        console.log("NOVO ID" + novoId)
+    novoId = listaLocalProdutos[listaLocalProdutos.length - 1].id + 1;
+  }, []);
 
-    },[]);
+  const [produto, setProduto] = useState({
+    id: novoId,
+    nome: "",
+    desc: "",
+    preco: "",
+    img: "",
+  });
 
-   return(
-    <><div>InserirProduto</div><div>
-           <form onSubmit={handleSubmit}>
-               <fieldset>
-                   <legend>Produto Selecionado</legend>
-                   <input type="hidden" name="id" value={produto.id} />
-                   <div>
-                       <label htmlFor="idProd">Nome do Produto</label>
-                       <input type="text" name="nome" id="idProd" onChange={handleChange} value={produto.nome} />
-                   </div>
-                   <div>
-                       <label htmlFor="idDesc">Descrição</label>
-                       <input type="text" name="desc" id="idDesc" onChange={handleChange} value={produto.desc} />
-                   </div>
-                   <div>
-                       <label htmlFor="idPreco">Preço</label>
-                       <input
-                           type="text"
-                           name="preco"
-                           id="idPreco"
-                           onChange={handleChange}
-                           value={produto.preco} />
-                   </div>
-                   <div>
-                       <button>EDITAR</button>
-                   </div>
-               </fieldset>
-           </form>
-       </div></>
-   ) 
+  const handleChange = (event) => {
+    //Destructuring
+    const { name, value } = event.target;
+    setProduto({ ...produto, [name]: value });
+  };
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+    fetch("http://localhost:5000/produtos",{
+        method: "POST",
+        body: JSON.stringify(produto),
+        headers:{
+            "Content-Type":"application/json",
+        }
+    })
+    .then((response)=> response.json())
+    .then((data)=>(console.log(data)))
+    .catch(error => console.log(error));
+
+     navigate("/produtos");
+  }
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Cadastrar Produto</legend>
+            <div>
+              <label htmlFor="idProd">Nome do Produto</label>
+              <input
+                type="text"
+                name="nome"
+                id="idProd"
+                onChange={handleChange}
+                value={produto.nome}
+              />
+            </div>
+            <div>
+              <label htmlFor="idDesc">Descrição</label>
+              <input
+                type="text"
+                name="desc"
+                id="idDesc"
+                onChange={handleChange}
+                value={produto.desc}
+              />
+            </div>
+            <div>
+              <label htmlFor="idPreco">Preço</label>
+              <input
+                type="text"
+                name="preco"
+                id="idPreco"
+                onChange={handleChange}
+                value={produto.preco}
+              />
+            </div>
+            <div>
+              <label htmlFor="idImg">Imagem</label>
+              <input
+                type="url"
+                name="img"
+                id="idImg"
+                onChange={handleChange}
+                value={produto.img}
+              />
+            </div>
+            <div>
+              <button>CADASTRAR</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
 }
